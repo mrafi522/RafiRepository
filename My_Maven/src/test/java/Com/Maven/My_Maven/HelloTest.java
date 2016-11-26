5e33ba4c4a0af3adb7f87c5206ad25587abf95e9
mrafi522 <mrafi522@gmail.com>
2016-11-26 00:53:46 -0500

    Initial checking program

---
+++ My_Maven/src/test/java/Com/Maven/My_Maven/HelloTest.java
@@ -0,0 +1,13 @@
+package Com.Maven.My_Maven;
+
+import org.testng.annotations.Test;
+
+public class HelloTest {
+
+	@Test
+	public void Mavennn(){
+		
+		System.out.println("Starting Mevan Rafi----");
+	}
+
+}
